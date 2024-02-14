8ee068ca19e204b64638ae0d96c2b16330ca51dd
Carlos Vaca <cmauricioec@gmail.com>
2024-02-13 21:02:20 -0500

    Add support for webp image format

--- backend/config.py
+++ backend/config.py
@@ -4,4 +4,4 @@ DEBUG = bool(int(os.environ.get("FLASK_DEBUG", 1)))
 UPLOAD_FOLDER = "uploads"
 OUTPUT_FOLDER = "outputs"
 WATERMARK_PATH = "imagined-with-ai.png"
-ALLOWED_EXTENSIONS = {"png", "jpg", "jpeg"}
+ALLOWED_EXTENSIONS = {"png", "jpg", "jpeg", "webp"}
